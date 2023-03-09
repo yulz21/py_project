54848c698331d4f4581b389856cba6b03c1da32a
yuliazhukova <yuliazhukova@hotmail.com>
2023-03-09 16:23:18 +0100

    import substract function

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from funcs import sum_
+from funcs import substact_
 
 if __name__ == "__main__":
-    result = sum_(4, 6)
-    print(result)
+    result_sum = sum_(4, 6)
+    print(result_sum)
+
+    result_subst = substact_(6, 3)
+    print(result_subst)
+
+
